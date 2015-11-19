75673585fc96965e80eb6ead5d10ad273b46e354
rachelfourq <rachel.lake@rocketmail.com>
2015-11-19 11:27:50 -0800

    added the .env

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,11 +1,16 @@
+require 'unirest'
+
 class MainController < ApplicationController
+
   def index
-  	response = Unirest.get "https://devru-instructables.p.mashape.com/list?limit=20&offset=0&sort=recent&type=id",
-  headers:{
-    "X-Mashape-Key" => "qNv5DeZglHmshdK7jNeqX3Y33WKpp1US0vpjsnLNyYfawqaTfR",
-    "Accept" => "application/json"
-  }
-  @results = JSON.parse(response)
+      response = Unirest.get "https://devru-instructables.p.mashape.com/list?limit=200&offset=0&sort=recent&type=id",
+	  headers:{
+	    "X-Mashape-Key" => "qNv5DeZglHmshdK7jNeqX3Y33WKpp1US0vpjsnLNyYfawqaTfR",
+	    "Accept" => "application/json"
+	  }
+	  # puts response.body.to_s
+	  @results = response.body.to_json
+	  # render :json => response
   end
 
 end
