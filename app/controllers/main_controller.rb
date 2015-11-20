58daf2d9d48b6cdfebf730471a3c360392e21a1a
rachelfourq <rachel.lake@rocketmail.com>
2015-11-19 21:32:03 -0800

    added search and playing around with query function - not totally working yet.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,12 +3,8 @@ require 'rest-client'
 class MainController < ApplicationController
 
   def index
-    q = 'language'
-response = RestClient.get 'http://www.khanacademy.org/api/v1/topic/' + q + '/videos'
-    @results = response.body.to_json
-    # render :json => response
-    @results = JSON.parse(response)
-    # [children][0]
+    
+     @lessons = Lesson.search(params[:search])
 
   end
 
