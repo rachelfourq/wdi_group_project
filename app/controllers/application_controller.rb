ca9289f7e23e011448c59b538c908a0b4c54b006
rachelfourq <rachel.lake@rocketmail.com>
2015-11-23 20:00:42 -0800

    working side nav. first two items in li are working. Left the others in for future. need to reformat rest of site to be responsive to nav... somehow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,8 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
+    @upload = Upload.new
+    @uploads = Upload.last(10).reverse
+
   end
 end
