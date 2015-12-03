5a5d850fafa31d1ad17bddc26490c0b298278df4
Haylee Nighbert <haylee.nigh@gmail.com>
2015-12-03 09:56:14 -0800

    Refactored/Updates to views

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,6 +1,4 @@
-
-
-  $(document).ready(function(){
+$(document).ready(function(){
   	console.log('working');
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
